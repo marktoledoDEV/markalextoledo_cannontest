bb1362e3586f9053fe655ada464b25a1fb532ebf
marktoledoDEV <markalex.toledo1@gmail.com>
2018-12-13 22:35:46 -0800

    project started

---
+++ markalextoledo_cannontest/Assets/Script/CannonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//CannonController is responsible for the funcionality of the cannon and and attaching those funcionality to keyboard bindings
+public class CannonController : MonoBehaviour 
+{
+	[Header("Cannon Properties")]
+	[SerializeField] private float horizontalRotationSpeed; //the speed of Cannon moving left and right
+	[SerializeField] private float verticalRotationSpeed;  //the speed of Cannon moving up and down
+	[SerializeField] private float fireRate; //how many cannons spawned per second
+
+	public enum playerInputOption { Horizontal, Vertical, Space }
+	[Header("Player Controls")]
+	[SerializeField] private playerInputOption horizontalAxisInput;
+	[SerializeField] private playerInputOption verticalAxisInput;
+	[SerializeField] private playerInputOption CannonFire;
+}
