ab56f071e411a3d9ab8a7f6fdc54ed2271152a8a
marktoledoDEV <markalex.toledo1@gmail.com>
2018-12-15 04:53:42 -0800

    shooting ranged spawner implemented

---
+++ markalextoledo_cannontest/Assets/Script/Spawners/BaseSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Contains the properties and functionality for all spawners
+public abstract class BaseSpawner : MonoBehaviour 
+{
+	[Header("Spawner Properties")]
+	[SerializeField] private GameObject spawneePrefab;
+	public int spawnAmount = 10; //howmuch spawneePrefab will be spawned;
+
+	private void Awake()
+	{
+		InitializeSpawner();
+	}
+
+	//Setup dependacies for spawners
+	protected virtual void InitializeSpawner() {	}
+
+	//quick function to spawn the spawner's spawneePrefab
+	public GameObject Spawn(Vector3 position, Quaternion rotation)
+	{
+		return Instantiate(spawneePrefab,position, rotation);
+	}
+		public GameObject Spawn(Vector3 position, Quaternion rotation, Transform parent)
+	{
+		return Instantiate(spawneePrefab,position, rotation,parent);
+	}
+}
---
+++ markalextoledo_cannontest/Assets/Script/Spawners/ShootingRangeSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//will spawn the desired game objects randomly throughout a grid.
+public class ShootingRangeSpawner : BaseSpawner 
+{
+	[Header("Shooting Range Spawner Properties")]
+	[SerializeField] private int gridHeight = 10;
+	[SerializeField] private int gridWidth = 10;
+	[SerializeField] private float spawnSpacing = 10.0f; //the space between each spawneePrefab
+
+	//private properties
+	private bool[,] SpawnGrid; //Spawngrid will be useed
+
+	//set up the dimensions of the spawn grid
+	protected override void InitializeSpawner()
+	{
+		ResetSpawnGrid();
+		GenerateShootingRange();
+	}
+
+	public void GenerateShootingRange()
+	{
+		for(int i = 0; i < spawnAmount; i++)
+		{
+			bool foundUnusedPosition = false;
+			while(foundUnusedPosition == false)
+			{
+				int rndHeight = Random.Range(0,gridHeight);
+				int rndWidth = Random.Range(0,gridWidth);
+
+				if(!SpawnGrid[rndWidth,rndHeight])
+				{
+					float xPos = rndWidth + spawnSpacing;
+					float yPos = rndHeight + spawnSpacing;
+					Vector3 spawnPosition = new Vector3(xPos, yPos, 0.0f);
+					GameObject spawnedTarget = Spawn(spawnPosition, transform.rotation,transform);
+					spawnedTarget.transform.localPosition = spawnPosition;
+					
+					SpawnGrid[rndWidth,rndHeight] = true;
+					foundUnusedPosition = SpawnGrid[rndWidth,rndHeight];
+				}
+			}
+		}
+	}
+
+	private void ResetSpawnGrid()
+	{
+		SpawnGrid = new bool[gridWidth, gridHeight];
+		for(int i = 0; i <gridWidth; i++)
+		{
+			for(int j = 0; j < gridHeight; j++)
+			{
+				SpawnGrid[i,j] = false;
+			}
+		}
+	}
+}
