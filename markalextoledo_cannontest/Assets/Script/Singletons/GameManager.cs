d82a6f0fe8664a4f41e8cc81b48734f338d43944
marktoledoDEV <markalex.toledo1@gmail.com>
2018-12-15 00:53:35 -0800

    GameManager and singleton skeleton made

---
+++ markalextoledo_cannontest/Assets/Script/Singletons/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> 
+{
+	
+	public CannonController player;
+
+	protected override void SingletonAwake()
+	{
+		Debug.Log(gameObject.name + " has been Initialized");
+	}
+}
---
+++ markalextoledo_cannontest/Assets/Script/Singletons/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+
+	public static T instance;
+
+	private void Awake()
+	{
+		if(instance != null)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			instance = (T)this;
+			SingletonAwake();
+
+		}
+	}
+
+	protected abstract void SingletonAwake();
+}
