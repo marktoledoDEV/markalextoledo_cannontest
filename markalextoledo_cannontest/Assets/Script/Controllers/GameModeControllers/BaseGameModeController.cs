5c0a14bf05c6fa08c79ba4e4ecbf23998478d8c9
marktoledoDEV <markalextoledo@gmail.com>
2018-12-16 15:12:06 -0600

    base game mode controller is defined

---
+++ markalextoledo_cannontest/Assets/Script/Controllers/GameModeControllers/BaseGameModeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//An easy way to define all the different types of gamemodes available
+public enum GameModesType { Horde, TargetPractice }
+
+public abstract class BaseGameModeController : MonoBehaviour
+{
+    public GameModesType GameMode;
+
+    private void Awake()
+    {
+        InitializeController();
+    }
+
+    private void Update()
+    {
+        UpdateController();
+    }
+
+    public abstract void SetupController();
+    public abstract void UpdateController();
+    public abstract void DesetupController();
+
+    private void InitializeController()
+    {
+        if(GameManager.instance != null)
+        {
+            GameManager.instance.GameControllerDictionary.Add(GameMode,this);
+        }
+
+        gameObject.SetActive(false);
+    }
+}
--- markalextoledo_cannontest/Assets/Script/Singletons/GameManager.cs
+++ markalextoledo_cannontest/Assets/Script/Singletons/GameManager.cs
@@ -5,10 +5,38 @@ using UnityEngine;
 //An easy way to access important references in the game scene
 public class GameManager : Singleton<GameManager> 
 {
-	public CannonController player;
+	public CannonController player; //a reference to the player for any class to access
+
+	//Store different gamecontrollers here
+	public Dictionary<GameModesType,BaseGameModeController> GameControllerDictionary = new Dictionary<GameModesType, BaseGameModeController>();
+	private BaseGameModeController currentActiveGameController = null;
 
 	protected override void SingletonAwake()
 	{
 		Debug.Log(gameObject.name + " has been Initialized");
+	}
+
+	//a getter for the currentActiveGameController
+	public BaseGameModeController GetCurretGameModeController()
+	{
+		return currentActiveGameController;
+	}
+
+	public void ChangeGameModes(GameModesType mode)
+	{
+		if(currentActiveGameController != null)
+		{
+			currentActiveGameController.DesetupController();
+		}
+
+		bool changeSuccessful = GameControllerDictionary.TryGetValue(mode, out currentActiveGameController);
+		if(changeSuccessful)
+		{
+			currentActiveGameController.SetupController();
+		}
+		else
+		{
+			Debug.LogError("CANNOT FIND GAMECONTROLLER: " + mode.ToString());
+		}
 	}
 }
